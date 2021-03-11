cf3fd0dd7bf0f1072d3049f7286199f605f701e2
Vicki Lee <leevicki01@gmail.com>
2021-03-11 12:16:32 -0800

    fix react router paths, add functionality to reactFileParser

--- src/backend/reactFileParser.js
+++ src/backend/reactFileParser.js
@@ -9,3 +9,36 @@ function please() {
 }
 
 please();
+
+const dev = window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
+const fiber = dev.getFiberRoots(1);
+
+console.log(dev.onCommitFiberRoot);
+
+// patching / rewriting the onCommitFiberRoot functionality
+dev.onCommitFiberRoot = (function (original) {
+  return function (...args) {
+    const fiberNode = args[1].current.child;
+    console.log('fiberNode', fiberNode);
+    const nodeName = fiberNode.type.name;
+    console.log('root node', nodeName);
+    getComponentNames(fiberNode);
+  };
+})(dev.onCommitFiberRoot);
+
+console.log('after change', dev.onCommitFiberRoot);
+
+// function that creates an object for each node
+
+const components = [];
+function getComponentNames(node) {
+  components.push(node.type.name);
+  if (node.child) {
+    getComponentNames(node.child);
+  }
+  if (node.sibling) {
+    // some code here
+    getComponentNames(node.sibling);
+  }
+}
+console.log('component names', components);
